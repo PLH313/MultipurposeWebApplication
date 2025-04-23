37e79aafec3bc61a3917d432e7911ccb2e731d42
PLH313 <hoangle0301@gmail.com>
2025-04-23 18:34:38 +0700

    Your commit message

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'], // For Google provider images
     },
+    eslint: {
+        ignoreDuringBuilds: ['/myproject']
+    },
+
 }
 
 module.exports = nextConfig
