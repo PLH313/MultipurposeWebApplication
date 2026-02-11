5d0ea3fd1d2539ecce8c2653ca5fcc8e9ddb5f0a
PLH313 <164978102+PLH313@users.noreply.github.com>
2026-02-11 21:17:51 +0700

    Beta

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,29 @@
-    import { NextResponse } from 'next/server'
-    import type { NextRequest } from 'next/server'
-    import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { getToken } from 'next-auth/jwt'
 
-    export async function middleware(request: NextRequest) {
-        const token = await getToken({ req: request })
-        const { pathname } = request.nextUrl
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ 
+        req: request, 
+        secret: process.env.NEXTAUTH_SECRET 
+    })
 
-        if (token && pathname.startsWith('/auth')) {
-            return NextResponse.redirect(new URL('/', request.url))
-        }
+    const { pathname } = request.nextUrl
 
-        if (!token && !pathname.startsWith('/auth')) {
-            return NextResponse.redirect(new URL('/auth/signin', request.url))
-        }
 
-        return NextResponse.next()
+    if (token && pathname.startsWith('/auth')) {
+        return NextResponse.redirect(new URL('/', request.url))
     }
 
-    export const config = {
-        matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+    if (!token && !pathname.startsWith('/auth')) {
+        const url = new URL('/auth/signin', request.url)
+        url.searchParams.set('callbackUrl', encodeURI(request.url))
+        return NextResponse.redirect(url)
     }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+}
