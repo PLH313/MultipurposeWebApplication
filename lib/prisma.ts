d0adb482346e5634fce6a954590ae7121f93b959
PLH313 <164978102+PLH313@users.noreply.github.com>
2026-02-11 19:39:40 +0700

    Test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
-﻿import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-    // eslint-disable-next-line no-var
-    var prisma: PrismaClient | undefined
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-const prisma = global.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
-export default prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
